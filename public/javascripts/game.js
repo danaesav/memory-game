f06c4605c31bced5fd3dbe4fbe86418185da4e1c
iliasmc <iliasmcauliffe@gmail.com>
2022-01-04 18:41:45 +0100

    made game functionality 🎮😂💯

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,12 +2,23 @@ var popUp = document.getElementById('popUp');
 var quitBtn = document.getElementById('quitBtn');
 var noBtn = document.getElementById('noBtn');
 var toDim = document.querySelectorAll(".dim");
+var yourScore = document.getElementById("yourScore");
+var timer = document.querySelector("header");
 var fronts = document.querySelectorAll(".front");
 var rears = document.querySelectorAll(".rear");
+var cards = document.querySelectorAll(".card");
 var images_names = ["3me.jpg", "aerospace.jpg", "architecture.jpg", "aula.jpg", "church.jpg", "EEMCS.jpg", "library.jpg", "castle.jpg", "station.jpg", "xtudelft.jpg", "andy.jpg", "nuna.jpg", "satellite.jpg", "ice.jpg", "vermeer.jpg", "pottery.jpg", "lake.jpg", "sunset.jpg", "christmas.jpg", "ww2.jpg"];
 
+let score = 0;
 
-// Make and shuffle array of 40 images 
+/////////// Update timer every second ////////////////
+for(let i=0; i<=500; i++){
+    setTimeout(function(){
+        timer.textContent = "Timer: " + i + "s";
+    }, 1000*i);
+}
+
+/////////// Make and shuffle array of 40 images ////////////////////
 var images = []
 for(let i=0; i<2; i++){
     for(let j=0; j<images_names.length; j++){
@@ -27,13 +38,12 @@ function shuffle(array){
     return array;
 }
 
-// Set the fronts
+//////////////// Set the initial images ///////////////////
 fronts.forEach(function(front){
     var icon = document.createElement("img");
-    icon.src = "images/flame3.png";
+    icon.src = "images/flame_card_not_found.png";
     front.appendChild(icon);
 })
-
 
 for(let i=0; i<images.length; i++){
     var icon = document.createElement("img");
@@ -41,7 +51,7 @@ for(let i=0; i<images.length; i++){
     rears[i].appendChild(icon);
 }
 
-
+//////////////// Quit button and pop up functionality ///////////
 function activatePopUp(){
     popUp.style.display='block';
     for(e of toDim){
@@ -58,10 +68,41 @@ function disactivatePopUp(){
 }
 noBtn.addEventListener("click", disactivatePopUp);
 
-fronts.forEach(function(front){
-    front.addEventListener("click", function(){
-        front.className="front slide";
-    });
-})
 
+//////////// Handling click on cards ////////////////
+let pair = []
+for(let i=0; i<fronts.length; i++){
+    fronts[i].addEventListener("click", function(){
+        console.log("Something was clicked");
+        if(pair.length<2){
+            fronts[i].classList.add("slide-up");
+            pair.push(i);
+        }
+        if(pair.length == 2){
+            if(images[pair[0]] == images[pair[1]]){
+                setTimeout(function(){
+                    yourScore.textContent = "Your score: " + (((++score) / 20) * 100) + "%";
+                    fronts[pair[0]].className = "front slide-down";
+                    fronts[pair[1]].className = "front slide-down";
 
+                    fronts[pair[0]].childNodes[0].src = "images/flame_card_found.png";
+                    fronts[pair[1]].childNodes[0].src = "images/flame_card_found.png";
+                    cards[pair[0]].className="card found";
+                    cards[pair[1]].className="card found";
+                    pair = [];
+                }, 2000);
+            }else{
+                setTimeout(function(){
+                    fronts[pair[0]].className = "front slide-down";
+                    fronts[pair[1]].className = "front slide-down";
+                }, 2000);
+                setTimeout(function(){
+                    fronts[pair[0]].className="front";
+                    fronts[pair[1]].className="front";
+                    pair = [];
+                }, 2500);
+            }
+        }
+        
+    })
+}
